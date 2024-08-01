d1a069bf4c7740528ee0bb6a30ccd3fa842296ad
ronaksarvaya <ronaksarvaiya2@gmail.com>
2024-08-01 21:15:57 +0530

    Update script.js

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,86 +1,66 @@
+let msg = document.querySelector(".display");
+let boxes = document.querySelectorAll(".btns");
 
-let msg=document.querySelector(".display")
-let boxes = document.querySelectorAll(".btns")
-// console.log(boxes)
+let Xturn = true;
 
-let Xturn=true;
+const winnPattern = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-const winnPattern=[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (Xturn) {
+            box.innerText = "X";
+            Xturn = false;
+        } else {
+            box.innerText = "0";
+            Xturn = true;
+        }
+        box.disabled = true;
+        winCheck();
+    });
+});
 
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
+function winCheck() {
+    let pos1, pos2, pos3;
+    for (let pattern of winnPattern) {
+        pos1 = boxes[pattern[0]].innerText;
+        pos2 = boxes[pattern[1]].innerText;
+        pos3 = boxes[pattern[2]].innerText;
 
-
-    [0,4,8],
-    [2,4,6]
-]
-
-boxes.forEach(
-    (box)=>{
-        box.addEventListener("click",()=>{
-            
-            if(Xturn){
-            box.innerText="X";
-            Xturn=false;
-            box.disabled=true
-            
-            
+        if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
+            if (pos1 === pos2 && pos2 === pos3) {
+                msg.innerText = `${pos1} won`;
+                boxes.forEach((box) => {
+                    box.disabled = true;
+                });
+                return; // Exit function after a win
             }
-            else{
-                box.innerText="0";
-                Xturn=true;
-                box.disabled=true
-               
-            }
-            winCheck()
-
-          
-        })
+        }
     }
-)
 
-//win chekcer
-
-function winCheck(){
-    let pos1,pos2,pos3;
-    for(let pattern of winnPattern){
-         pos1=boxes[pattern[0]].innerText
-         pos2=boxes[pattern[1]].innerText
-         pos3=boxes[pattern[2]].innerText
-
-         if(pos1!=""&&pos2!=""&&pos3!=""){
-            if(pos1===pos2 && pos2===pos3 && pos1===pos3){
-                msg.innerText=`${pos1} won`
-                boxes.forEach(
-                    (box)=>{
-                        box.disabled=true;})
-                        
-                    }
-            else{
-                msg.innerText=`Draw`
-            }
-                }
-                
-        
+    if (isBoardFull()) {
+        msg.innerText = "It's a draw!";
     }
-    
 }
 
+function isBoardFull() {
+    return Array.from(boxes).every(box => box.innerText !== "");
+}
 
-//reset
+let reset = document.getElementById("resetbtn");
 
-let reset= document.getElementById("resetbtn")
-
-reset.addEventListener("click",()=>{
-    //
-    boxes.forEach(
-        (box)=>{
-            box.innerText=""
-            box.disabled=false
-        }
-    )
-})
+reset.addEventListener("click", () => {
+    boxes.forEach((box) => {
+        box.innerText = "";
+        box.disabled = false;
+    });
+    msg.innerText = ""; // Clear the message when resetting the game
+});
